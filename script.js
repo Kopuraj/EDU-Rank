5166a9d278ca5e95a9b8959ec91b21ed97d3d55f
Kopuraj <thiruchelvamkopuraj@gmail.com>
2025-01-11 16:03:50 +0530

    create login form

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('homeButton').addEventListener('click', function() {
+    const optionsDiv = document.getElementById('options');
+    optionsDiv.classList.toggle('hidden');
+});
