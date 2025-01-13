5a7a983ecaedff2f29e1002656cd00bd42b2b97e
Kopuraj <thiruchelvamkopuraj@gmail.com>
2025-01-13 23:18:11 +0530

    pages inside the web page

---
+++ frontscript.js
@@ -0,0 +1,10 @@
+document.getElementById('adlogin').addEventListener('click', function() {
+    window.location.href = 'login form/login.html'; 
+   
+});
+
+document.getElementById('about').addEventListener('click', function() {
+    window.location.href = 'aboutus page/aboutus.html'; 
+   
+});
+
