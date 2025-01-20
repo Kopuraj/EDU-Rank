ce0455fcbe0492ce7030ffaa7a58d2ca4b0bf0ae
Kopuraj <thiruchelvamkopuraj@gmail.com>
2025-01-20 07:18:47 +0530

    clink pageshange

--- aboutuspage/aboutus.js
+++ aboutuspage/aboutus.js
@@ -1,20 +1,20 @@
-document.getElementById('aboutus.html/contact').addEventListener('click', function() {
-    window.location.href = 'contactus/contactus/contactus.html'; 
-   
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('home').addEventListener('click', function() {
+        window.location.href = 'dashboard.html'; 
+    });
+
+    document.getElementById('about').addEventListener('click', function() {
+        window.location.href = 'aboutuspage/aboutus.html'; 
+    });
+
+    document.getElementById('contact').addEventListener('click', function() {
+        window.location.href = 'contactus/contactus/contactus.html'; 
+    });
+
+    document.getElementById('adlogin').addEventListener('click', function() {
+        window.location.href = 'login form/login.html'; 
+    });
 });
 
-document.getElementById('about').addEventListener('click', function() {
-    window.location.href = 'aboutuspage/aboutus.html'; 
-   
-});
-
-document.getElementById('home').addEventListener('click', function() {
-    window.location.href = 'dashboard.html'; 
-   
-});
-
-document.getElementById('adlogin').addEventListener('click', function() {
-    window.location.href = 'login form/login.html'; 
-   
-});
 
