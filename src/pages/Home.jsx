77fb65c40d8fb961c91eac6e7e04513e15726ac3
Kopuraj <thiruchelvamkopuraj7@gmail.com>
2025-01-31 18:35:40 +0530

    feat: re-edit the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,31 +7,74 @@ const Home = () =>{
       <header>
         <div className="header">
           <h1 className="logo">EDU-Rank</h1>
-          <div>
+          <div className="buton-group">
             <button>Home</button>
-            <button>About</button>
-            <button>Contact</button>
-            <button>Logout</button>
+            <button >About</button>
+            <button >Contact</button>
+            <button >Admin Login</button>
           </div>
         </div>
-      </header>
-      <div className="main-content-header">
-        <h1>Admin Dashboard</h1>
-        <h2>
-          <a href="addmark.php">⇒ Add Marks of Student</a>
-        </h2>
-        <h2>
-          <a href="updatemark.php">⇒ Update Student Marks</a>
-        </h2>
-        <h2>
-          <a href="deleteform.php">⇒ Delete the Marks of Student</a>
-        </h2>
-        <h2>
-          <a href="allstudentdata.php">⇒ Data of all Students</a>
-        </h2>
-        <h2>
-          <a href="usermassage.php">⇒ Messages by Student</a>
-        </h2>
+        </header>
+        
+      
+
+      <div className="body">
+      <img className="image" src="/images/front image1.png" alt="Front Display" />
+        <div className="sub-body">
+          <h2 className="text">
+            Transforming <br /> Student Performance with <br /> Accurate Reports
+          </h2>
+          
+          <div className="main-content-header">
+            <form method="post" action="result.php">
+              <h2 className="search">REGISTER AND GET YOUR RESULT HERE!</h2>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th className="standered1">Name</th>
+                    <td className="standard2">
+                      <input type="text" name="name" required className="box2" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="name1">Register Number</th>
+                    <td className="name2">
+                      <input type="text" name="rollno" required className="box1" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <th className="standered1">Grade</th>
+                    <td className="standard2">
+                      <input type="text" name="grade" required className="box2" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td align="center" colSpan="2">
+                      <button type="submit" name="submit" className="submit">
+                        RESULT
+                      </button>
+                      <button type="submit" name="register" className="submit">
+                        REGISTER
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </form>
+          </div>
+        </div>
+        
+
+      </div>
+
+      <div className="footer-section-contact">
+        <h3>Contact us</h3>
+        <p>Email: kopurajEdu@gmail.com</p>
+        <p>Phone: +94 741125425</p>
+      </div>
+
+      <div className="footer-bottom">
+        &copy; 2025 TK.com | Designed by Kopuraj T.
       </div>
     </div>
   );
